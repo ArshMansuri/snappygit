e08179d75cdbfae6c4ff730ad6d65b57554651d2
MansuriArsh <mansuriarsh44@gmail.com>
2023-03-04 21:27:10 +0530

    first commit 12

--- app.js
+++ app.js
@@ -9,9 +9,7 @@ const PORT = process.env.PORT || 6020
 
 
 
-app.use(cors({
-    origin: "https://social-snappy.web.app"
-}));
+
 
 const {connectDataBase} = require('./db/conn');
 connectDataBase();
@@ -24,10 +22,13 @@ app.use(express.json({limit: "50mb"}));
 app.use(express.urlencoded({extended: true}));
 
 
-// app.use(cors(origin = 'https://social-snappy.web.app/'));
 //---------------import router ----------
 const user = require('./routes/user');
 const post = require('./routes/post');
+
+app.use(cors({
+    origin: "https://social-snappy.web.app"
+}));
 
 
 //---------------use router -------------
