dd87c6b76b0ad9ddf5a385c27fe7231ec4a0976a
MansuriArsh <mansuriarsh44@gmail.com>
2023-03-04 21:49:19 +0530

    first commit 15

--- app.js
+++ app.js
@@ -22,7 +22,8 @@ app.use(express.json({limit: "50mb"}));
 app.use(express.urlencoded({extended: true}));
 
 
-app.use(cors());
+// app.use(cors());
+app.options('*', cors())
 //---------------import router ----------
 const user = require('./routes/user');
 const post = require('./routes/post');
